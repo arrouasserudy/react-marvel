3d110c00258fc464680b19b03468248d9e4befc3
arrouasserudy <arrouasserudy@hotmail.com>
2019-10-23 23:30:12 +0300

    organize files & create first screen

---
+++ src/components/App/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Marvel</h1>
+      <div className='App-boxes'>
+        <div className='App-box'>
+          <div>
+            Title 1
+          </div>
+          <div>
+            body 1
+          </div>
+        </div>
+        <div className='App-box'>
+          <div>
+            Title 2
+          </div>
+          <div>
+            body 2
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
