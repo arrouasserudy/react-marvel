b2101e821b26d0758026fba96dd710a7b18d61e9
arrouasserudy <arrouasserudy@hotmail.com>
2019-10-29 00:16:58 +0200

    set image color by isHero

--- src/components/CharacterCard/CharacterCard.js
+++ src/components/CharacterCard/CharacterCard.js
@@ -6,16 +6,26 @@ const DEFAULT_DESCRIPTION = 'Un personnage marvel'
 
 const getHeroText = isHero => `C'est un ${isHero ? 'hero' : 'vilain'}`
 
+const getColor = isHero => {
+    if (isHero === true) {
+        return 'blue'
+    } else if (isHero === false) {
+        return  'red'
+    }
+    return 'grey'
+}
+
 const CharacterCard = props => {
     const { name, description, isHero } = props
+
     return (
         <div className={styles.container}>
             <div className={styles.top}>
-                <div className={styles.image}/>
+                <div className={`${styles.image} ${styles[getColor(isHero)]}`}/>
                 <div className={styles.name}>{name}</div>
             </div>
             <div>{description}</div>
-            <div>{getHeroText(isHero)}</div>
+            <div className={getColor(isHero)}>{getHeroText(isHero)}</div>
         </div>
     )
 }
