35b4c7e916b1da7c158c678b8bcb1c539364d452
arrouasserudy <arrouasserudy@hotmail.com>
2019-10-28 23:06:23 +0200

    add 'key'

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,25 +1,24 @@
-import React from 'react';
-import styles from './App.module.css';
-import characters from "../../utils/characters";
-import CharacterCard from "../CharacterCard/CharacterCard"
+import React from 'react'
+import styles from './App.module.css'
+import characters from '../../utils/characters'
+import CharacterCard from '../CharacterCard/CharacterCard'
 
 const App = () => {
-  return (
-    <div className={styles.container}>
-      <h1>Marvel</h1>
-      <div className={styles.boxes}>
-        {
-            characters.map(item =>
-                <CharacterCard
-                name={item.name}
-                description={item.description}
-                isHero={item.isHero}
-                />
-            )
-        }
-      </div>
-    </div>
-  );
+    return (
+        <div className={styles.container}>
+            <h1>Marvel</h1>
+            <div className={styles.boxes}>
+                {characters.map(item => (
+                    <CharacterCard
+                        key={item.name}
+                        name={item.name}
+                        description={item.description}
+                        isHero={item.isHero}
+                    />
+                ))}
+            </div>
+        </div>
+    )
 }
 
-export default App;
+export default App
