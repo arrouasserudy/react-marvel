f47d6861016a11b9f02a052d074c508f0ff80e01
arrouasserudy <arrouasserudy@hotmail.com>
2019-11-06 00:41:22 +0200

    transform CharacterCard to class component

--- src/components/CharacterCard/CharacterCard.js
+++ src/components/CharacterCard/CharacterCard.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import styles from '../CharacterCard/CharacterCard.module.css'
 
@@ -8,41 +8,65 @@ const getColor = isHero => {
     if (isHero === true) {
         return 'blue'
     } else if (isHero === false) {
-        return  'red'
+        return 'red'
     }
     return 'grey'
 }
 
-const CharacterCard = props => {
-    const { name, description, isHero, fullScreen, setFullScreenId, id } = props
+class CharacterCard extends Component {
+    renderFullScreenCard = () => {
+        const { name, description, isHero, setFullScreenId } = this.props
 
-    const FullScreenCard = (
-        <div className={styles.fullScreenContainer}>
-            <div className={`${styles.fullScreenImage} ${styles[getColor(isHero)]}`}/>
-            <div className={styles.fullScreenBody}>
-                <div className={styles.name}>{name}</div>
+        return (
+            <div className={styles.fullScreenContainer}>
+                <div
+                    className={`${styles.fullScreenImage} ${
+                        styles[getColor(isHero)]
+                    }`}
+                />
+                <div className={styles.fullScreenBody}>
+                    <div className={styles.name}>{name}</div>
+                    <div className={styles.description}>{description}</div>
+                </div>
+                <div
+                    className={styles.close}
+                    onClick={() => setFullScreenId(null)}
+                >
+                    X
+                </div>
+            </div>
+        )
+    }
+
+    renderCard = () => {
+        const { name, description, isHero, setFullScreenId, id } = this.props
+        return (
+            <div
+                className={styles.container}
+                onClick={() => setFullScreenId(id)}
+            >
+                <div className={styles.top}>
+                    <div
+                        className={`${styles.image} ${
+                            styles[getColor(isHero)]
+                        }`}
+                    />
+                    <div className={styles.name}>{name}</div>
+                </div>
                 <div className={styles.description}>{description}</div>
             </div>
-            <div className={styles.close} onClick={() => setFullScreenId(null)}>X</div>
-        </div>
-    )
+        )
+    }
 
-    const Card = (
-        <div className={styles.container} onClick={() => setFullScreenId(id)}>
-            <div className={styles.top}>
-                <div className={`${styles.image} ${styles[getColor(isHero)]}`}/>
-                <div className={styles.name}>{name}</div>
-            </div>
-            <div className={styles.description}>{description}</div>
-        </div>
-    )
-
-    if (fullScreen) {
-        return FullScreenCard
-    } else if (fullScreen === null) {
-        return Card
-    } else {
-        return null
+    render() {
+        const { fullScreen } = this.props
+        if (fullScreen) {
+            return this.renderFullScreenCard()
+        } else if (fullScreen === null) {
+            return this.renderCard()
+        } else {
+            return null
+        }
     }
 }
 
