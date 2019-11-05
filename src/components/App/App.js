6a7bb12acc6503bcc4944d560a74495e16df0432
arrouasserudy <arrouasserudy@hotmail.com>
2019-11-05 22:57:48 +0200

    add constructor & state function binding

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,11 +4,36 @@ import TitleBanner from '../TitleBanner/TitleBanner'
 import CardList from '../CardList/CardList'
 
 class App extends React.Component {
+
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            counter: 0
+        }
+    }
+
+    // Marche
+    incremente = () => {
+        this.setState({counter: this.state.counter + 1})
+    }
+
+    // Ne marche pas
+    decremente() {
+        this.setState({counter: this.state.counter - 1})
+    }
+
     render() {
+        const {counter} = this.state
         return (
             <div className={styles.container}>
                 <TitleBanner />
-                <CardList />
+                {/*<CardList />*/}
+                <div className={styles.counter}>
+                    <button onClick={this.incremente}>Incrementer</button>
+                    <div>{counter}</div>
+                    <button onClick={this.decremente}>Decrementer</button>
+                </div>
             </div>
         )
     }
