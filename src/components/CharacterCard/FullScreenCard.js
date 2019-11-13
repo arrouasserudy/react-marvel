47b908c6241c511fb5cd4d504857159ac673303a
arrouasserudy <arrouasserudy@hotmail.com>
2019-11-13 22:06:15 +0200

    add comment editor

--- src/components/CharacterCard/FullScreenCard.js
+++ src/components/CharacterCard/FullScreenCard.js
@@ -3,19 +3,54 @@ import PropTypes from 'prop-types'
 import styles from '../CharacterCard/CharacterCard.module.css'
 
 class FullScreenCard extends Component {
+    state = {
+        haveComment: false,
+        comment: ''
+    }
+
+    onSubmit = e => {
+        e.preventDefault()
+        this.setState({haveComment: true})
+    }
+
+    onEdit = () => {
+        this.setState({haveComment: false})
+    }
+
+    onChange = e => {
+        this.setState({comment: e.target.value})
+    }
+
     render() {
         const { name, description, color, setFullScreenId } = this.props
+        const { haveComment, comment } = this.state
 
         return (
             <div className={styles.fullScreenContainer}>
-                <div
-                    className={`${styles.fullScreenImage} ${
-                        styles[color]
-                    }`}
-                />
+                <div className={`${styles.fullScreenImage} ${styles[color]}`} />
                 <div className={styles.fullScreenBody}>
-                    <div className={styles.name}>{name}</div>
-                    <div className={styles.description}>{description}</div>
+                    <div>
+                        <div className={styles.name}>{name}</div>
+                        <div className={styles.description}>{description}</div>
+                    </div>
+                    <div className={styles.commentContainer}>
+                        {!haveComment ? (
+                            <div>
+                                <form onSubmit={this.onSubmit}>
+                                    <input
+                                        placeholder="Type your comment..."
+                                        onChange={this.onChange}
+                                        value={comment}
+                                    />
+                                </form>
+                            </div>
+                        ) : (
+                            <div className={styles.comment}>
+                                <div>{comment}</div>
+                                <button onClick={this.onEdit}>Edit</button>
+                            </div>
+                        )}
+                    </div>
                 </div>
                 <div
                     className={styles.close}
@@ -34,6 +69,5 @@ FullScreenCard.propTypes = {
     fullScreen: PropTypes.bool,
     setFullScreenId: PropTypes.func,
 }
-
 
 export default FullScreenCard
