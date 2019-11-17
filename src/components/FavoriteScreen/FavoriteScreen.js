02992fd963e4140da9b030a37fb52f0e3a814d47
arrouasserudy <arrouasserudy@hotmail.com>
2019-11-17 23:30:25 +0200

    intro to axios

--- src/components/FavoriteScreen/FavoriteScreen.js
+++ src/components/FavoriteScreen/FavoriteScreen.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import styles from './FavoriteScreen.module.css'
+import axios from 'axios'
 
 class FavoriteScreen extends React.Component {
+    onClick = async () => {
+        const response = await axios.get('http://dummy.restapiexample.com/api/v1/employees')
+        console.log(response)
+    }
+
     render() {
         return (
             <div className={styles.container}>
                 Favorites
+                <button onClick={this.onClick}>Test AXIOS</button>
             </div>
         )
     }
