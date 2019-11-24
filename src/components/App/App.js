fff9d2837b387bd7ab4228ffe8e6426172be90c3
arrouasserudy <arrouasserudy@hotmail.com>
2019-11-25 01:23:46 +0200

    provide store to react app

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,23 +5,31 @@ import {
     Route,
 } from "react-router-dom"
 
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+
 import Layout from "../Layout/Layout"
 import SearchScreen from "../SearchScreen/SearchScreen"
 import FavoriteScreen from "../FavoriteScreen/FavoriteScreen"
 import NotFoundScreen from "../NotFoundScreen/NotFoundScreen"
+import reducers from '../../reducers/index'
+
+const store = createStore(reducers)
 
 class App extends React.Component {
    render() {
         return (
-            <Router>
-                <Layout>
-                    <Switch>
-                        <Route exact path="/" component={SearchScreen}/>
-                        <Route path="/favorites" component={FavoriteScreen}/>
-                        <Route component={NotFoundScreen} />
-                    </Switch>
-                </Layout>
-            </Router>
+            <Provider store={store}>
+                <Router>
+                    <Layout>
+                        <Switch>
+                            <Route exact path="/" component={SearchScreen}/>
+                            <Route path="/favorites" component={FavoriteScreen}/>
+                            <Route component={NotFoundScreen} />
+                        </Switch>
+                    </Layout>
+                </Router>
+            </Provider>
         )
     }
 }
