507b878e665a026ab111295209c00b646fbf2a6c
arrouasserudy <arrouasserudy@hotmail.com>
2019-11-25 01:24:20 +0200

    connect redux to the Timer component

--- src/components/Timer/Timer.js
+++ src/components/Timer/Timer.js
@@ -1,17 +1,22 @@
 import React, {Component} from 'react'
+import {connect} from 'react-redux'
+import {getTime} from '../../selectors/timer'
+import {setTime} from '../../actions/timer'
 
 class Timer extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            timer: 0,
+            // timer: 0,
             show: true,
         }
     }
 
     componentDidMount() {
+        const {setTime} = this.props
         this.interval = setInterval(() => {
-            this.setState(state => ({timer: state.timer + 1}))
+            // this.setState(state => ({timer: state.timer + 1}))
+            setTime(this.props.timer + 1)
         }, 1000)
     }
 
@@ -24,10 +29,10 @@ class Timer extends Component {
         this.setState({show: false})
     }
 
-    formatNumber = number => number < 10 ? `0${number}`: number
+    formatNumber = number => number < 10 ? `0${number}` : number
 
     displayTime = () => {
-        const {timer} = this.state
+        const {timer} = this.props
         const seconds = timer % 60
         const minutes = Math.floor(timer / 60) % 60
         const hours = Math.floor(timer / (60 * 60)) % 24
@@ -45,4 +50,12 @@ class Timer extends Component {
     }
 }
 
-export default Timer
+const mapStateToProps = state => ({
+    timer: getTime(state)
+})
+
+const mapDispatchToProps = {
+    setTime,
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Timer)
